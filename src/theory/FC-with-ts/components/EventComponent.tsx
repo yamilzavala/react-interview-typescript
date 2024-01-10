04acbbc8c188fb9c1583156457c43573c6ca42bd
Yamil Zavala <zavalayamil9@gmail.com>
2024-01-10 09:15:29 -0300

    Event components with ts

--- src/theory/FC-with-ts/components/EventComponent.tsx
+++ src/theory/FC-with-ts/components/EventComponent.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 const EventComponent: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+        if(!inputRef.current){
+            return;
+        }
+        inputRef.current.focus();
+    },[])
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         console.log(e.target.value)
     }
 
+    const handleDrag = (e: React.DragEvent<HTMLDivElement>) => {
+        console.log(e)
+    }
+
     return (
         <div>
-            <input onChange={handleChange}/>
+            <input ref={inputRef} onChange={handleChange}/>
+            <div draggable onDragStart={handleDrag}>Drag me!</div>
         </div>
     );
 };
